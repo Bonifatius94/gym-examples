34c9b9a4c81a6724850e254478535ad93659de6d
Marco Tröster <marco@troester-gmbh.de>
2022-07-25 23:41:17 +0200

    implement model update routine

--- training.py
+++ training.py
@@ -7,6 +7,8 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras.losses import MeanSquaredError
+from tensorflow.keras.optimizers import Adam
 
 
 State = int
@@ -16,7 +18,7 @@ SARSExperience = Tuple[State, Action, float, State, bool]
 
 class Env:
     def __init__(self):
-        self.env = gym.make('FrozenLake-v1', map_name='8x8', new_step_api=True)
+        self.env = gym.make('FrozenLake-v1', map_name='4x4', new_step_api=True)
         self.state = self.env.reset()
 
     def apply_action(self, action: Action) -> SARSExperience:
@@ -46,8 +48,9 @@ class ExponentialDecay:
 class Agent:
     def __init__(self, nn: tf.keras.Model=None):
         self.nn: tf.keras.Model = self._create_neural_network() if not nn else nn
-        self.learn_rate = 0.1
-        self.exploration_rate = ExponentialDecay(1.0, 0.01, 10000)
+        self.loss_fn = MeanSquaredError()
+        self.optimizer = Adam(learning_rate=0.02)
+        self.exploration_rate = ExponentialDecay(1.0, 0.03, 50000)
         self.gamma = 0.99
 
     def select_action(self, state: State):
@@ -55,8 +58,23 @@ class Agent:
         return random.randint(0, 3) if explore else self._pick_best_action(state)
 
     def update_model(self, sars_exp: SARSExperience):
-        # TODO: implement the training step
-        pass
+        state_before, action, reward, state_after, is_done = sars_exp
+        q_after = self._predict_single(state_after)
+        q_target = np.zeros(4)
+        terminal = 1.0 if is_done else self.gamma * max(q_after)
+        q_target[action] = reward + terminal
+
+        x = np.expand_dims(np.expand_dims(state_before, axis=0), axis=0)
+        y = np.expand_dims(q_target, axis=0)
+
+        with tf.GradientTape() as tape:
+            pred = self.nn(x)
+            loss_mask = tf.expand_dims(tf.one_hot(np.array(action), 4), axis=0)
+            masked_pred = tf.multiply(pred, loss_mask)
+            loss = self.loss_fn(y, masked_pred)
+
+        grads = tape.gradient(loss, self.nn.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads, self.nn.trainable_variables))
 
     def _pick_best_action(self, state: State) -> Action:
         return np.argmax(self._predict_single(state))
@@ -70,11 +88,12 @@ class Agent:
     @staticmethod
     def _create_neural_network() -> tf.keras.Model:
         model = Sequential([
-            Dense(128),
-            Activation('relu'),
-            Dense(256),
-            Activation('relu'),
+            Dense(64),
+            Activation('sigmoid'),
+            Dense(16),
+            Activation('sigmoid'),
             Dense(4),
+            Activation('sigmoid'),
         ])
 
         model.build((None, 1))
@@ -87,7 +106,6 @@ def train_episode(env: Env, agent: Agent) -> Tuple[float, int]:
     steps = 0
 
     while not is_done:
-        # print(f'step {steps}')
         action = agent.select_action(env.state)
         sars_exp = env.apply_action(action)
         agent.update_model(sars_exp)
@@ -112,16 +130,24 @@ class AverageMetric:
         self.data_points = []
 
 
+class RandomAgent:
+    def select_action(self, _: State):
+        return random.randint(0, 3)
+
+    def update_model(self, sars_exp: SARSExperience):
+        pass
+
+
 def main():
     env = Env()
     agent = Agent()
-    log_interval = 10
+    log_interval = 100
 
     print('starting training')
     avg_rewards = AverageMetric()
     avg_steps_per_episode = AverageMetric()
 
-    for episode in range(1, 10001):
+    for episode in range(1, 50001):
         reward_sum, steps = train_episode(env, agent)
         avg_rewards.append(reward_sum)
         avg_steps_per_episode.append(steps)
